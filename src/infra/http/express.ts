a9708a287bf16ed2ce7bf6b62ea522e5ca0a7975
Clinton Brito <clintoncsb@gmail.com>
2024-07-15 21:05:48 -0300

    ✨ feat: create `ExpressAdapter` and `ParkingLotController`

--- src/infra/http/express.ts
+++ src/infra/http/express.ts
@@ -1,16 +1,11 @@
 import Express from "express";
-import GetParkingLot from "../../core/usecase/GetParkingLot";
-import ParkingLotRepositorySQL from "../repository/ParkingLotRepositorySQL";
+import ExpressAdapter from "../../adapter/ExpressAdapter";
+import ParkingLotController from "../../controller/ParkingLotController";
 
 
 const app = new Express();
 
-app.get("/parkingLots/:code", async function (req, res) {
-    const parkingLotRepositorySQL = new ParkingLotRepositorySQL();
-    const getParkingLot = new GetParkingLot(parkingLotRepositorySQL);
-    const parkingLot = await getParkingLot.execute(req.params.code);
-    res.json(parkingLot);
-})
+app.get("/parkingLots/:code", ExpressAdapter.create(ParkingLotController.getParkingLot));
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
